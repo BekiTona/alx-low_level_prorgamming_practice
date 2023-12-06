2f2578ed89f7bf3305bc4a71f84a43dc1e8b69b4
Bereket Alemayehu <bereketalemayehu15@gmail.com>
2023-12-06 11:21:29 -0800

    a fucntion that check the last digit of the intiger variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - the code checks if the last digit of an intger has different values
+ *
+ * Return: always (0)
+ **/
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+				
+					return (0);
+}
